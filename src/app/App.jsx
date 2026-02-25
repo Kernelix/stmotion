ffd9dbdd06a04d8a86de04873d170209d8a8f96d
Stanislav Tutov <STutov@nefco.ru>
2026-02-25 23:24:24 +0300

    Revert "удалил фолбэк"
    This reverts commit e0da68715d94f7eefa2cf1be137f063358e6d303.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -73,8 +73,8 @@ export default function App() {
 
   return (
     <div className="app-shell relative min-h-[100svh] min-h-[100dvh] overflow-x-hidden text-ink-900">
-      <ThreeScene className="pointer-events-none fixed inset-x-0 top-0 z-0 h-[100svh] h-[100dvh] w-screen" onModelsReady={onModelsReady} />
-      <div className="pointer-events-none fixed inset-x-0 top-0 z-[1] h-[100svh] h-[100dvh] w-screen">
+      <ThreeScene className="fixed inset-0 z-0 pointer-events-none" onModelsReady={onModelsReady} />
+      <div className="pointer-events-none fixed inset-0 z-[1]">
         <div className="absolute inset-0 bg-[radial-gradient(circle_at_top,_rgba(255,255,255,0.22),_rgba(246,246,242,0.02))]" />
         <div className="absolute inset-0 opacity-[0.1] [background-size:220px_220px] [background-image:linear-gradient(to_right,rgba(21,21,21,0.05)_1px,transparent_1px),linear-gradient(to_bottom,rgba(21,21,21,0.05)_1px,transparent_1px)] [mask-image:radial-gradient(ellipse_at_center,black_40%,transparent_82%)]" />
       </div>
